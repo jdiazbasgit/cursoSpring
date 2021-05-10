55e7eebb67200b6c6717b5fd381245ea9e0b33df
Tu Nombre <you@example.com>
2021-05-10 19:57:22 +0200

    final

--- src/teatros/ConciertoSolistaSpring.java
+++ src/teatros/ConciertoSolistaSpring.java
@@ -1,10 +1,18 @@
 package teatros;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import musicos.Solista;
+
 public class ConciertoSolistaSpring {
 
 	public static void main(String[] args) {
 		
 
+			ApplicationContext ctx= new ClassPathXmlApplicationContext("spring.xml");
+			Solista pepe=(Solista) ctx.getBean("solista");
+			pepe.tocar();
 	}
 
 }
